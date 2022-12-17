346221767dbc2d62c53b0a33da5c9c60c982c5e0
Kamil <kamit2002@wp.pl>
2022-12-17 12:49:20 +0100

    Signed-off-by: Kamil <kamit2002@wp.pl>

---
+++ lab5/5.1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Podaj liczbe elementow tablicy: ";
+    cin >> n;
+
+    vector<int> tab(n);
+    int max = 0, maxIndex = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> tab[i];
+        if(tab[i] > max) {
+            max = tab[i];
+            maxIndex = i;
+        }
+    }
+
+    cout << "Najwieksza wartoscia elementu tablicy jest " << max << " o indeksie " << maxIndex << ".";
+
+    return 0;
+}
---
+++ lab5/5.2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int rows;
+    cout << "Podaj liczbe wierszy tablicy: ";
+    cin >> rows;
+
+    int tab[2][rows];
+    cout << "Podaj elementy tablicy:" << endl;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < 2; j++) {
+            cin >> tab[i][j];
+        }
+    }
+
+    cout << "Tablica przed przeksztalceniem:" << endl;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < 2; j++) {
+            cout << tab[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < rows; i++) {
+        int p = tab[i][0];
+        tab[i][0] = tab[i][1];
+        tab[i][1] = p;
+    }
+    cout << "Tablica po przeksztalceniu:" << endl;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < 2; j++) {
+            cout << tab[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
