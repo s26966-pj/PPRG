e3d26052d36989ba47f1c56312bc5fd29ebb79e7
Kamlit <kamlit2002@wp.pl>
2022-12-27 13:22:29 +0100

    5.2 fix

--- lab5/5.2.cpp
+++ lab5/5.2.cpp
@@ -8,7 +8,7 @@ int main()
     cout << "Podaj liczbe wierszy tablicy: ";
     cin >> rows;
 
-    int tab[2][rows];
+    int tab[rows][2];
     cout << "Podaj elementy tablicy:" << endl;
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < 2; j++) {
@@ -29,7 +29,7 @@ int main()
         tab[i][0] = tab[i][1];
         tab[i][1] = p;
     }
-    
+
     cout << "Tablica po przeksztalceniu:" << endl;
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < 2; j++) {
